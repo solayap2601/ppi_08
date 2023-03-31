0d907643e8cb8cf72ba7de46fae80df21d59799d
alejoec03 <juespinosac@unal.edu.co>
2023-03-31 13:51:56 -0500

    ajustes aplicacion

--- AppPPI/Tu_profe/urls.py
+++ AppPPI/Tu_profe/urls.py
@@ -1,15 +1,32 @@
-from django.urls import path
-from . import views
+"""AppPPI URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from Tu_profe.views import login_view, register_view, facultades_view, carrera_view, asignatura_view, profesor_view, search_view, calificar, perfil 
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-    path('facultades/', views.facultades_view, name='facultades'),
-    path('carrera/<int:carrera_id>/', views.carrera_view, name='carrera'),
-    path('asignatura/<int:asignatura_id>/', views.asignatura_view, name='asignatura'),
-    path('profesor/<int:profesor_id>/<int:curso_id>/', views.profesor_view, name='profesor'),
-    path('search/', views.search_view, name='search'),
-    path('calificar/<int:profesor_id>/<int:curso_id>/', views.calificar, name='calificar'),
-    path('perfil/', views.perfil, name='perfil'),
+    path('', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('facultades/', facultades_view, name='facultades'),
+    path('carrera/<int:carrera_id>/', carrera_view, name='carrera'),
+    path('asignatura/<int:asignatura_id>/', asignatura_view, name='asignatura'),
+    path('profesor/<int:profesor_id>/<int:curso_id>/', profesor_view, name='profesor'),
+    path('search/', search_view, name='search'),
+    path('calificar/<int:profesor_id>/<int:curso_id>/', calificar, name='calificar'),
+    path('perfil/', perfil, name='perfil'),
 ]
+
 

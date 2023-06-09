cfaaf83138beb8c715c5300d4ba71b8114309eda
RHMAX <115115870+RHMAX@users.noreply.github.com>
2023-06-09 12:38:22 -0500

    se hizo comentario a todo el codigo

--- tuProfe/tuProfe/urls.py
+++ tuProfe/tuProfe/urls.py
@@ -1,32 +1,39 @@
-"""tuProfe URL Configuration
+# tuProfe URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# El `urlpatterns` lista las URLs y las asocia a las vistas correspondientes.
+# Para obtener mas informacion, consulta la documentacion de Django:
+# https://docs.djangoproject.com/en/4.1/topics/http/urls/
+
+# Importa de Django
 from django.contrib import admin
 from django.urls import path
-from tuProfe_app.views import home, calificar, buscar, docente, guardar, login, register, politicasp
 from django.contrib.auth import views as auth_views
 
+# Esta parte Importa la vista
+from tuProfe_app.views import (
+    home, calificar, buscar, docente, guardar, login, register, politicasp
+)
+
+# Contiene todas las rutas (URLs) disponibles en la aplicacion
 urlpatterns = [
+    # Ruta para acceder a la interfaz de administración de Django.
     path('admin/', admin.site.urls),
+    # Ruta para la pagina de inicio
     path('', home, name='home'),
-    path('politicasp/', politicasp, name='politicasp'),
-    path('registro/', register, name='registro'),
-    path('login/', login, name='login'),
-    path('calificar/', calificar, name= "calificar"),
-    path('buscar/', buscar, name="buscar"),
-    path('docente/<int:id_docente>/<int:id_materia>/<int:pagina>', docente),
-    path('guardar/', guardar, name="guardar"),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout')
+    # Ruta de politicas y privacidad para la pagina
+    path('politicasp/', politicasp, name='politicasp'),  
+    # Ruta para el registro de usuarios
+    path('registro/', register, name='registro'),  
+    # Ruta de inicio de sesion
+    path('login/', login, name='login'),  
+    # Ruta de calificacion
+    path('calificar/', calificar, name='calificar'),  
+    # Ruta de busqueda de la pagina
+    path('buscar/', buscar, name='buscar'),  
+    # Ruta para mirar los docentes
+    path('docente/<int:id_docente>/<int:id_materia>/<int:pagina>/', docente),
+    # Ruta para guardar la calificacion
+    path('guardar/', guardar, name='guardar'),  
+    # Ruta de cerrar sesion en la pagina
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'), 
 ]
